3480cbe38c8dbe30a77d7468b86d680201c36f6c
erx-e <figueroa0cristian@gmail.com>
2023-03-14 01:42:33 -0500

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // API_URL: 'https://localhost:44387',
-  API_URL: 'http://pets-dev.us-east-1.elasticbeanstalk.com/',
+  API_URL: 'http://pets-dev.us-east-1.elasticbeanstalk.com',
   AWS_REGION: 'us-east-1',
   AWS_KEY_ID: 'AKIA5QZLTSGGKZDHYFGM',
   AWS_KEY_SECRET: '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',
