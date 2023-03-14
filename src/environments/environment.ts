065694a6aaaf9502fe8b09878b9e5823452efd94
erx-e <figueroa0cristian@gmail.com>
2023-03-14 01:36:25 -0500

    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   // API_URL: 'https://localhost:44387',
-  API_URL: 'https://petslighthouseapi.online',
+  API_URL: 'http://pets-dev.us-east-1.elasticbeanstalk.com/',
   AWS_REGION: 'us-east-1',
-  AWS_KEY_ID: 'AKIA5QZLTSGGJZHXFGEC',
-  AWS_KEY_SECRET: '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',
+  AWS_KEY_ID: 'AKIA5QZLTSGGKZDHYFGM',
+  AWS_KEY_SECRET: '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',
   BUCKET_URL: 'https://petslighthouse.s3.amazonaws.com',
   BUCKET_NAME: 'petslighthouse',
 };
