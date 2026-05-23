55946228a3ed9b3e63f99fcbdb0f12b7d44c87a7
erxick <figueroa0cristian@gmail.com>
2026-05-23 10:51:01 -0500

    https protocol in api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // API_URL: 'https://localhost:44387',
-  API_URL: 'http://petlighthouse.somee.com',
+  API_URL: 'https://petlighthouse.somee.com',
   // URL pública del bucket S3, solo para MOSTRAR imágenes. No es secreto.
   // Las credenciales de AWS viven únicamente en el backend.
   BUCKET_URL: 'https://petslighthouse.s3.amazonaws.com',
